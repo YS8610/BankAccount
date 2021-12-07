ae26be65e708acea91240a8219b911c3261d2dfd
YS_workPC <ong_ys86@yahoo.com.sg>
2021-12-07 10:40:45 +0800

    adds tests for bankaccount

--- src/test/java/com/day2/BankAccountTest.java
+++ src/test/java/com/day2/BankAccountTest.java
@@ -6,9 +6,38 @@ import org.junit.Test;
 
 public class BankAccountTest
 {
+
+    BankAccount acct1 = new BankAccount("TFIP"); //Create bank account with name only (initial bank bal is set to 0f)
+    BankAccount acct2 = new BankAccount("TFIP",1000f); // create bank acct with name and account balance of 1000f initialized
+
     @Test
-    public void shouldAnswerWithTrue()
+    public void testGetAccountName()
     {
-        assertTrue( true );
+        assertTrue( acct1.getName().equals("TFIP") );
+        assertTrue( acct1.getaccountBalance()==0f );
     }
+    
+    @Test
+    public void testGetAccountBalance()
+    {
+        assertTrue( acct2.getName().equals("TFIP") );
+        assertTrue( acct2.getaccountBalance() == 1000f );
+    }
+
+    @Test
+    public void testDeposit()
+    {
+        float beforeDeposit = acct1.getaccountBalance();
+        acct1.deposit(1000f, "071221");
+        assertTrue(acct1.getaccountBalance()==beforeDeposit+1000f);
+    }
+
+    @Test
+    public void testWithdraw()
+    {
+        float beforeWithdraw = acct2.getaccountBalance();
+        acct2.withdraw(500f,"071221");
+        assertTrue(acct2.getaccountBalance()==beforeWithdraw-500f);
+    }
+
 }
